f8c60eee9ebed4f68347a4140150a934d0c2664c
JoaoTrajano <joao.trajanosouza@gmail.com>
2024-04-05 20:25:05 -0300

    feature: created at nmew module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
 import { UserModule } from '@/modules/user/user.module'
+import { AuthService } from './modules/auth/application/services/auth.service'
 
 @Module({
   imports: [EnvConfigModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
