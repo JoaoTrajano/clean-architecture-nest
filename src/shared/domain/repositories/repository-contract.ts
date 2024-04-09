9ce10cc338253fefdffc64edbf674c1df06342c8
JoaoTrajano <joao.trajanosouza@gmail.com>
2024-04-09 17:23:31 -0300

    feature: repository - refect

--- src/shared/domain/repositories/repository-contract.ts
+++ src/shared/domain/repositories/repository-contract.ts
@@ -2,8 +2,8 @@ import { Entity } from '../entities/entity'
 
 export interface RespositoryInterface<E extends Entity> {
   insert(data: E): Promise<boolean>
-  findById(id: string): Promise<E>
+  findById(id: string): Promise<E | null>
   findAll(): Promise<E[]>
-  update(data: E): Promise<boolean>
-  delete(id: string): Promise<boolean>
+  update(data: E): Promise<boolean | null>
+  delete(id: string): Promise<boolean | null>
 }
